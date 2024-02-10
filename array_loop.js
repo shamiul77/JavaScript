c0f42e6ec6b899802ae9f6ca3e7ace79bd2b42cb
Shamiul Islam <118125555+shamiul77@users.noreply.github.com>
2024-02-10 17:07:31 +0600

    Create array_loop.js

---
+++ array_loop.js
@@ -0,0 +1,18 @@
+
+
+
+
+    let number = [1,2,3,4,5,6,7,8,9,10,11];{        //access the array using loop
+        for(let i=0; i<number.length; i++){
+            console.log(number[i]);
+        }
+        
+    }
+
+
+    let x = ["apple", "mango", "banana", "Lemon", "orange"];    //slice+loop
+y = x.slice(0,3);
+console.log(y);for
+for(let i=0; i<y.length; i++){
+    console.log(y[i]);
+}
